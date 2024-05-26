192d98a13486212b63b1bf6a7b9fa8fc2cc5c070
Traube1000101 <girlscoutcookies420@gmx.net>
2024-05-26 18:33:19 +0200

    first commit

---
+++ concat-config.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+const baseFile = "base.toml"
+
+func watchExec(file string, function func()) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				if event.Has(fsnotify.Write) {
+					function()
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("Error: ", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-make(chan struct{})
+}
+
+func concatConfig(files []string) {
+	fmt.Println("Concatenating \""+baseFile+"\" to following files: ", strings.Join(files, "\n"))
+
+	base, err := os.ReadFile(baseFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		transient, err := os.ReadFile(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		baseStr := string(base)
+		transientStr := string(transient)
+
+		beginingMarker := "# Begining of " + baseFile
+		endMarker := "# End of " + baseFile
+
+		if strings.Contains(transientStr, beginingMarker) && strings.Contains(transientStr, endMarker) {
+			startIndex := strings.Index(transientStr, beginingMarker) + len(beginingMarker)
+			endIndex := strings.Index(transientStr, endMarker)
+			transientStr = transientStr[:startIndex] + "\n" + baseStr + "\n" + transientStr[endIndex:]
+		} else {
+			transientStr += "\n" + beginingMarker + "\n" + baseStr + "\n" + endMarker
+		}
+
+		err = os.WriteFile(file, []byte(transientStr), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func main() {
+	files := []string{"transient.toml", "main.toml"}
+
+	var argLength int = len(os.Args)
+	if argLength > 1 {
+		if os.Args[1] == "-h" || os.Args[1] == "--help" {
+			log.Println("Usage: concat-config [option] <files>")
+			os.Exit(0)
+		} else if os.Args[1] == "-w" || os.Args[1] == "--watch" {
+			if argLength > 2 {
+				files = os.Args[2:]
+			}
+			concatConfig(files)
+			fmt.Println("Watching \"" + baseFile + "\"...")
+			watchExec(baseFile, func() {
+				concatConfig(files)
+			})
+		} else {
+			files = os.Args[1:]
+			concatConfig(files)
+		}
+	} else {
+		concatConfig(files)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Traube1000101/concat-config
+
+go 1.22.3
+
+require github.com/fsnotify/fsnotify v1.7.0
+
+require golang.org/x/sys v0.4.0 // indirect
